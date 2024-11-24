f4611740b1400753456d014fec985c4c8c5fdfd1
Abhinandan-Mane <abhinandan.mane04@gmail.com>
2024-11-24 11:40:25 +0530

    Adding Section_Assignment folder to Section-1to-6-CPP_Basics

---
+++ Section-1to6-CPP_Basics/Section_Assignment/favourite_num.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+    int num;
+    std::cout << "Enter your favourite number between 10 and 200: ";
+    std::cin >> num;
+
+    std:: cout << "Amazing it is my favourite too !" << std::endl;
+    std :: cout<< "No!! really " << num << " is not !";
+
+}
